5b48fd894d65397724c54fe49e5922e85b574516
shenj <576410448@qq.com>
2015-09-24 19:42:02 +0800

    1

---
+++ News/NewsTests/AA.h
@@ -0,0 +1,13 @@
+//
+//  AA.h
+//  News
+//
+//  Created by qianfeng on 15/9/24.
+//  Copyright © 2015年 LiJiangTao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AA : NSObject
+
+@end
---
+++ News/NewsTests/AA.m
@@ -0,0 +1,13 @@
+//
+//  AA.m
+//  News
+//
+//  Created by qianfeng on 15/9/24.
+//  Copyright © 2015年 LiJiangTao. All rights reserved.
+//
+
+#import "AA.h"
+
+@implementation AA
+
+@end
